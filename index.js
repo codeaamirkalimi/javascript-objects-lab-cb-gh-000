f11942613ae22212d3aa6fce2e7a20620e424814
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-14 08:42:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   var newObj = Object.assign({}, object, {[key]: value});
   return newObj;
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
